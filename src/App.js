eeb23737c66ac0b128ed467b71f93af5b55e247d
Rmesh Kumar <rameshsee08@gmail.com>
2023-07-31 18:50:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  const increment = (value) => {
+    setCount(value);
+  };
+
+  const decrement = (value) => {
+    setCount(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box> <Typography sx={{ fontSize: "20px", fontWeight: 700, my: 5 }}>Counter App</Typography> </Box>
+      <Box sx={{ display: "flex" }}>
+        <Typography sx={{ fontSize: "20px", fontWeight: 700, marginRight: 5, marginTop: 10 }}>
+          {count}
+        </Typography>
+        {count > 0 && (
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={() => {
+              setCount(0);
+            }}
+            sx={{ height: "30px", marginTop: 10 }}
+          >
+            {" "}
+            Reset{" "}
+          </Button>
+        )}
+      </Box>
+
+      <Box sx={{ marginTop: 20 }}>
+        <Button
+          variant="contained"
+          onClick={() => increment(count + 2)}
+          sx={{
+            borderRadius: "2px",
+            marginRight: 5,
+            textTransform: "capitalize",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Increment
+        </Button>
+        <Button
+          disabled={count > 2 ? false : true}
+          variant="contained"
+          onClick={() => decrement(count - 3)}
+          sx={{
+            borderRadius: "2px",
+            textTransform: "capitalize"
+          }}
+        >
+          Decrement
+        </Button>
+      </Box>
+    </Box>
   );
 }
-
-export default App;
